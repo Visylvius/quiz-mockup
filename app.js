c2a6a5d559cc9a601f5f6edd238ca3f31e5db474
Loren Barrick <Lorenbarrick@gmail.com>
2015-07-16 17:51:14 -0700

    starting quiz mock-up

---
+++ app.js
@@ -0,0 +1,10 @@
+var questions = [
+  new Question("Who was the first President of the United States?", [ "George Washington", "Thomas Jefferson" ], "George Washington"),
+  new Question("What is the answer to the Ultimate Question of Life, the Universe, and Everything?", ["pi", "42"], "42")
+];
+
+//Create Quiz
+var quiz = new Quiz(questions);
+
+//Display Quick
+QuizUI.displayNext();
