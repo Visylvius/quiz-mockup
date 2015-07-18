361d81ca4e5203b5ec7c843e48014aebe6881bfd
Loren Barrick <Lorenbarrick@gmail.com>
2015-07-17 19:20:59 -0700

    added more questions

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var questions = [
   new Question("Who was the first President of the United States?", [ "George Washington", "Thomas Jefferson" ], "George Washington"),
-  new Question("What is the answer to the Ultimate Question of Life, the Universe, and Everything?", ["pi", "42"], "42")
+  new Question("What is the answer to the Ultimate Question of Life, the Universe, and Everything?", ["pi", "42"], "42"),
+  new Question("What is your favorite color?", ["green", "blue"], "green")
 ];
 
 //Create Quiz
